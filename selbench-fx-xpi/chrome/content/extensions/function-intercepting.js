0f8dd1c8910c23eae855fba2395959fb8cb2687f
chris <deadletter@aol.com>
2013-09-14 10:49:48 -0400

    commenting

--- selbench-fx-xpi/chrome/content/extensions/function-intercepting.js
+++ selbench-fx-xpi/chrome/content/extensions/function-intercepting.js
@@ -5,16 +5,19 @@
   */
   $$.fnStack = [];
 
+  // replace the specified function, saving the original function on a stack
   $$.interceptPush = function(targetObj, targetFnName, fn) {
     var frame = { targetObj: targetObj, targetFnName: targetFnName, savedFn: targetObj[targetFnName] };
     $$.fnStack.push(frame);
     targetObj[targetFnName] = fn;
   };
+  // restore the most recent function replacement
   $$.interceptPop = function() {
     var frame = $$.fnStack.pop();
     frame.targetObj[frame.targetFnName] = frame.savedFn;
   };
 
+  // replace the specified function, but then restore the original function as soon as it is call
   $$.interceptOnce = function(targetObj, targetFnName, fn) {
     $$.interceptPush(targetObj, targetFnName, function(){
       $$.interceptPop(); // un-intercept

9571ff5f51f282c23afc1c42034d53097dfd5c07
refactoror <deadletter@aol.com>
2015-01-09 17:36:32 -0500

    simplify server adapter

--- build/createSelbenchXpi.bat
+++ build/createSelbenchXpi.bat
@@ -12,25 +12,23 @@ set BUILD_DIR=%~dp0
 set ROOT=%BUILD_DIR%..
 
 :: parse SelBench version # from its install.rdf
-call :S_GET_ADDON_VER ..\selbench-fx-xpi
+call :S_GET_ADDON_VER %BUILD_DIR%\..\selbench-fx-xpi
 set SU_VER=%_ver%
 
 echo SelBench: %SU_VER%
 
-
 :: create SelBench xpi
-pushd %BUILD_DIR%\..\selbench-fx-xpi
+pushd "%BUILD_DIR%..\selbench-fx-xpi"
 del "%ROOT%\selbench-%SU_VER%-fx.xpi"
 zip -r "%ROOT:\=/%/../selbench-%SU_VER%-fx.xpi" * -x@"%BUILD_DIR%xpi-excludes.lst"
 popd
 
+pushd "%BUILD_DIR%"
+
 :: assemble user-extensions.js file
 CALL createSelbenchUserExtensions.cmd
 
-:: create minified version of user-extensions.js
-"%JAVA_HOME%\bin\java" -jar "yuicompressor-2.4.8.jar" ^
-   ../user-extensions.js ^
--o ../user-extensions-min.js
+popd
 
 ENDLOCAL
 
--- selbench-fx-xpi/chrome/content/extensions/name-space.js
+++ selbench-fx-xpi/chrome/content/extensions/name-space.js
@@ -2,7 +2,10 @@
 var selbench = { name: "SelBench" };
 
 (function($$){
-	
+  $$.seleniumEnv = "ide";
+  // global scope alias
+  $$.globalContext = this;
+
   /* Starting with FF4 lots of objects are in an XPCNativeWrapper,
    * and we need the underlying object for == and for..in operations.
    */

70302fef546973fcaeb6bc822aad23f5efde3c75
refactoror <deadletter@aol.com>
2015-02-03 20:49:06 -0500

    get opera 27 browser working & keep by-browser _results.html

--- build/run-testsuites.cmd
+++ build/run-testsuites.cmd
@@ -1,4 +1,7 @@
 SETLOCAL
+:: Dependencies:
+::   cygwin sed
+
 @echo off
 
 pushd "%~dp0"
@@ -9,8 +12,9 @@ set UE=user-extensions-min
 CALL createSelbenchUserExtensions.cmd
 
 :: for each browser, run each test suite
-CALL :s_run_suites firefox
-CALL :s_run_suites googlechrome
+CALL :s_run_suites "*firefox"
+CALL :s_run_suites "*googlechrome"
+CALL :s_run_suites "*opera"
 del *.heredoc
 
 popd
@@ -31,32 +35,37 @@ goto :eof
     echo.>> lib\user-extensions.js
   )
 
-  CALL :s_run_suite %1 ..\selbenchTests\_SelBench-regression.html
-  CALL :s_run_suite %1 ..\selbenchTests\negativeTests\_SelBench-regression-negative.html
+  CALL :s_run_suite "%~1" ..\selbenchTests\_SelBench-regression.html
+  CALL :s_run_suite "%~1" ..\selbenchTests\negativeTests\_SelBench-regression-negative.html
 goto :eof
 
 :s_run_suite
-:: <browser-name> <test-suite-html>
+:: <browser-spec> <test-suite-html>
+  :: parse browser name, (eliminate leading *, and path if any)
+  FOR /F "tokens=1 delims=*^ " %%B IN ("%~1") DO set BROWSER_SPEC=%%B
   @echo on
   "%JAVA_HOME%\bin\java" ^
     -jar "lib\selenium-server-standalone-2.44.0.jar" ^
-    -debug ^
     -singleWindow ^
+    -debug ^
     -log logs\server.log ^
     -logLongForm ^
     -browserSideLog ^
     -userExtensions lib/user-extensions.js ^
-    -htmlSuite "*%1" ^
-    "http://www.google.com" ^
-    "%2" "%~p2\_results.html"
+    -htmlSuite "%~1" ^
+    "http://refactoror.net/" ^
+    "%2" ^
+    "%~d2%~p2\_results_%BROWSER_SPEC%.html"
   @echo off
-  CALL "%~p2\_results.html"
+  sed "/Please update to WebDriver ASAP/d" "%~d2%~p2\_results_%BROWSER_SPEC%.html" > "%~d2%~p2\_results_%BROWSER_SPEC%.tmp"
+  del "%~d2%~p2\_results_%BROWSER_SPEC%.html"
+  ren "%~d2%~p2\_results_%BROWSER_SPEC%.tmp" "_results_%BROWSER_SPEC%.html"
+  CALL "%~d2%~p2\_results_%BROWSER_SPEC%.html"
 goto :eof
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 TBD browsers
-CALL :s_run_suites iexplore
-CALL :s_run_suites opera
-CALL :s_run_suites safari
+CALL :s_run_suites "*iexplore"
+CALL :s_run_suites "*safari"
 
-    -browserSideLog ^
+    -proxyInjectionMode ^
